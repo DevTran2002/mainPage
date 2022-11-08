60360ccb2cb9640a895abbaae100e9fbdc988dc2
TranNgoi <tranngoihdnv@gmail.com>
2022-11-08 23:22:36 +0700

    fix pomodoro

--- js/timeClock.js
+++ js/timeClock.js
@@ -9,28 +9,32 @@ let pomodoro = document.querySelector(".pomodoro");
 let inputTimePomodoro = pomodoro.querySelector('.setTimePomodoro');
 let inputTimeShort = pomodoro.querySelector('.setTimeShort');
 let inputTimeLong = pomodoro.querySelector('.setTimeLong');
+var mute = pomodoro.querySelector('.pomodoro-mute');
 
+var messageClock = document.querySelector(".message-clock");
 
 // value
 var statusTime = true;          //trạng thái hidden or close
 var activeHidden = true;
-var breakTime  = 1;
+var breakTime  = 0;
 var activeStart = true;
 var s,m;
 const audio = new Audio();
         audio.src = "https://docs.google.com/uc?id=1U002Nc1diUPDQuEUpI9dj5EI1bI35_yv";
-        audio.loop;
-//out pomodoro
+const audioRing = new Audio()        ;
+        audioRing.src = "https://docs.google.com/uc?id=1iZPoYhO25G2qm-bph67hknz9IAlalosL";
+        
+        //out pomodoro
 pomodoro.querySelector('.out-pomodoro').onclick = function(){
         if(confirm('Bạn chắc chắn đóng Pomodoro không?')){
                 statusTime = false;
-                breakTime = 1;
-                document.querySelector(".message-clock").innerHTML = '0 : 0';
+                messageClock.innerHTML = '25 : 0';
                 countDow(s, m, statusTime);
                 pomodoro.setAttribute("style", "display: none");
                 eventPomodoro.classList.remove("activeEventClick-menu");
         }     
 }
+
 
 // button play pomodoro
 pomodoro.querySelector('.starTimePomodoro').onclick = function(){
@@ -48,6 +52,9 @@ pomodoro.querySelector('.starTimePomodoro').onclick = function(){
         }
                 
 }
+
+
+
 //pause pomodoro
 pomodoro.querySelector('.pauseTimePomodoro').onclick = function(){
         alert('Tạm dừng pomodoro');
@@ -88,6 +95,7 @@ function setBreakLong(m){
 
 
 
+
 //sự kiện khi bấm pomodoro-menu
 eventPomodoro.onclick = function () {
 
@@ -95,13 +103,11 @@ eventPomodoro.onclick = function () {
         if (activeHidden) {
                 pomodoro.setAttribute("style", " transform: scale(100%)" , "display: block");
                 document.querySelector('.block-timetable').setAttribute("style", " transform: scale(0)" , "display: none");
+
                 document.querySelector('.c-containter').setAttribute("style", "display: none");
                 document.querySelector('.menu').classList.remove("action");
-
-                eventPomodoro.classList.add("activeEventClick-menu");
                 activeHidden = false;
 
-                document.querySelector('.c-containter').setAttribute("style", "display: none");
         } 
         else{
                 pomodoro.setAttribute("style","transform: scale(0)", "display: none");
@@ -109,44 +115,61 @@ eventPomodoro.onclick = function () {
         }
 };
 
+mute.addEventListener('click', function(event){
+        if(confirm("Bạn có mốn tắt nhạc không")){
+                audio.pause();
+                audioRing.pause();
+        }
+})
+
+
 //hàm hiển thị và show time
 function countDow(s, m, statusTime) {
+                if(breakTime == 0){
+                        m = setTime(m);
+                        s=0;
+                        breakTime++;
+                }
 
-  var messageClock = document.querySelector(".message-clock");
-
-        if (statusTime) {               // trạng thái hidden
-                if (s == -1) {
+                if (s == -1 & m >= 1) {
                         m --;
                         s = 59;
-                }       
-                else if (m == 0 & s == 0  & breakTime % 2 == 1) {
-                        m = setTime(m);
-                        s = 0;
-                        breakTime ++;
-                }
-                else if(m == 0 & s == 0  & breakTime % 2 == 0 & breakTime != 8){
-                        audio.play();
-                        if(confirm("Bạn có mốn tắt nhạc không")){
-                                audio.pause();
+                }  
+        if (statusTime) {               // trạng thái hidden   (show active)   
+                console.log(breakTime);
+                audio.loop;
+                audioRing.loop;
+                if(s == 0 & m == 0 & breakTime %2 ==0 ){
+                        if(breakTime != 0){
+                                audioRing.play();
                         }
+                        m = setTime(m);
+                        s=0;
+                        console.log("time out");
+                        breakTime++;
+
+                }
+                else if(s  == 0 & m == 0 & breakTime % 2 == 1 & breakTime != 7)    {
+                        audio.play();
                         m = setBreakShort(m);
                         s = 0;
-                        breakTime ++;
+                        breakTime++;
+
+                        console.log("time break shot");
                 }
-                else if(m == 0 & s == 0 & breakTime == 8){
+                else if(s == 0 & m == 0 & breakTime == 7){
                         audio.play();
-                        if(confirm("Bạn có mốn tắt nhạc không")){
-                                audio.pause();
-                        }
                         m = setBreakLong(m);
                         s = 0;
-                        breakTime = 1;
+                        breakTime = 0;
+                        console.log("time break long");
                 }
         }
+        
         // điều kiện trạng thái out 
-       else if (statusTime == false) {
+       else {
                 clearTimeout(timeout);
-                count = 0;
+                breakTime =0;
                 return false;
         }
 
@@ -156,5 +179,6 @@ function countDow(s, m, statusTime) {
         timeout = setTimeout(function () {
                         s--;
                         countDow(s, m, statusTime);
+                        
                         }, 1000);
 }
